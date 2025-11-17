d4c7a1ea8eb934f204bc30608e676da2e8cd7910
SimonH93 <simon@ssmt.de>
2025-11-17 19:55:28 +0100

    Session_Info debug

--- main.py
+++ main.py
@@ -49,6 +49,9 @@ for key, value in os.environ.items():
 SESSION_PARTS.sort(key=lambda x: x[0])
 SESSION_BASE64 = "".join([part[1] for part in SESSION_PARTS])
 
+logging.info(f"DEBUG: SESSION_BASE64 Länge: {len(SESSION_BASE64)}")
+logging.info(f"DEBUG: SESSION_BASE64 Start: {SESSION_BASE64[:10]}... Ende: {SESSION_BASE64[-10:]}")
+
 if SESSION_BASE64:
     logging.info("Telethon Session Content gefunden und aus %d Teilen zusammengesetzt.", len(SESSION_PARTS))
 else:
